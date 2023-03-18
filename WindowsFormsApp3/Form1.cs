594db819e4c5e7ef749aa63355ebfc31275428f2
thimira28 <thimiraakash328@gmail.com>
2023-03-18 17:31:44 +0530

    Add project files.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ( textBox1.Text == "Admin" && textBox2.Text == "123456")
+            {
+                //Create an object from Homepage class
+                Homepage homepage = new Homepage();
+                //Display the Homepage form after the clicking login button
+                homepage.Show();
+                this.Hide();
+            } else
+            {
+                MessageBox.Show("Incorrect Username or Password","Alert",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+           
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp3/Homepage.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Homepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Homepage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Homepage";
+            this.Load += new System.EventHandler(this.Homepage_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
